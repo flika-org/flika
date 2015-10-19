5531626f750300a49ef49380cd35fd1dfa788368
Brett Settle <brettjsettle@gmail.com>
2015-10-19 16:00:46 -0700

    DBScan process file for Density Scanning algorithm
    File I/O, gui, and scanning functions for use in the density based scanning program

---
+++ process/dbscan_.py
@@ -0,0 +1,172 @@
+
+from __future__ import (absolute_import, division,print_function, unicode_literals)
+from future.builtins import (bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
+
+import numpy as np
+import pyqtgraph as pg
+import sys, os, time
+import global_vars as g
+from process.BaseProcess import BaseProcess, BaseDialog
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from sklearn.cluster import DBSCAN
+from scipy.spatial import ConvexHull
+from window3d import Window3D
+import pyqtgraph.opengl as gl
+
+#cluster, open_scatter, save_scatter, save_clusters, export_distances, export_nearest_distances
+
+class Cluster(BaseProcess):
+	"""cluster(epsilon, minPoints, minNeighbors=1)
+	This takes three values used to cluster coordinates with the Density Based Scanning algorithm
+	
+	Parameters:
+		| epsilon (float) -- The maximum distance between neighboring points.
+		| minPoints (int) -- The minimum number of points to be considered a cluster
+		| minNeighbors (int) -- The minimum neighboring points to include a point in a cluster
+	Returns:
+		List of lists of clustered points
+	"""
+	def __init__(self):
+		pass
+
+	def __call__(self, epsilon, minP, minNeighbors=1):
+		g.m.statusBar().showMessage('Clustering %d points...' % len(g.m.currentWindow.scatterPoints))
+		scanner = DBSCAN(eps = epsilon, min_samples=minNeighbors)
+		db = scanner.fit(g.m.currentWindow.scatterPoints)
+		count = len(set(db.labels_)) - (1 if -1 in db.labels_ else 0)
+		clusters = []
+		for i in range(count):
+			cl = np.array([g.m.currentWindow.scatterPoints[j] for j in np.where(db.labels_ == i)[0]])
+			if len(cl) >= minP:
+				clusters.append(cl)
+		g.m.currentWindow.reset()
+		g.m.statusBar().showMessage('%d clusters found.' % len(clusters))
+		self.clusters = clusters
+		self.cluster_id = 0
+		self.showCluster()
+		g.m.currentWindow.view.keyPressEvent = self.keyPressed
+
+	def hideCluster(self):
+		g.m.currentWindow.view.removeItem(self.cluster_mesh)
+		del self.cluster_mesh
+
+	def showCluster(self):
+		g.m.statusBar().showMessage('Plotting Cluster %d of %d. Point count: %d' % (self.cluster_id, len(self.clusters), len(self.clusters[self.cluster_id])))
+		ch = ConvexHull(self.clusters[self.cluster_id])
+		md = gl.MeshData(vertexes=ch.points, faces=ch.simplices)
+		if hasattr(self, 'cluster_mesh'):
+			self.cluster_mesh.setMeshData(meshdata=md)
+		else:
+			self.cluster_mesh = gl.GLMeshItem(meshdata=md, drawFaces=False, drawEdges=True, color=(0, 255, 0, 255))
+			g.m.currentWindow.view.addItem(self.cluster_mesh)
+		g.m.currentWindow.moveTo(np.average(self.clusters[self.cluster_id], 0))
+
+	def keyPressed(self, e):
+		if e.key() == 45:
+			self.cluster_id = (self.cluster_id - 1) % len(self.clusters)
+			self.showCluster()
+		elif e.key() == 61:
+			self.cluster_id = (self.cluster_id + 1) % len(self.clusters)
+			self.showCluster()
+		else:
+			super(gl.GLViewWidget, g.m.currentWindow.view).keyPressEvent(e)
+	
+	def gui(self):
+		epsiSpin=QDoubleSpinBox()
+		minPSpin = QSpinBox()
+		minNeighborsSpin = QSpinBox()
+		if g.m.currentWindow is not None:
+			epsiSpin.setValue(g.m.epsilonSpin.value())
+			minPSpin.setValue(g.m.minPointsSpin.value())
+			minNeighborsSpin.setValue(g.m.minNeighborsSpin.value())
+		items = []
+		items.append({'name':'epsilon','string':'Epsilon','object':epsiSpin})
+		items.append({'name':'minP','string':'Minimum Points','object':minPSpin})
+		items.append({'name':'minNeighbors','string':'Minimum Neighbors','object':minNeighborsSpin})
+		items.append({'name':'preview','string':'Preview','object':QCheckBox()})
+		super().gui()
+
+	def preview(self):
+		epsilon=self.getValue('epsilon')
+		minP=self.getValue('minP')
+		minNeighbors=self.getValue('minNeighbors')
+		preview=self.getValue('preview')
+		if preview:
+			g.m.currentWindow.reset()
+			self(epsilon, minP, minNeighbors)
+		else:
+			g.m.currentWindow.reset()
+cluster = Cluster()
+
+def save_scatter_gui():
+	if g.m.currentWindow is None:
+		return False
+	filename=g.m.settings['filename']
+	directory=os.path.dirname(filename)
+	if filename is not None and directory != '':
+		filename= QFileDialog.getSaveFileName(g.m, 'Save Scatter', directory, '*.txt')
+	else:
+		filename= QFileDialog.getSaveFileName(g.m, 'Save Scatter', '*.txt')
+	filename=str(filename)
+	if filename=='':
+		return False
+	else:
+		save_scatter(filename)
+
+def save_clusters(filename):
+	pass
+		
+def save_scatter(filename):
+	g.m.statusBar().showMessage('Saving Scatter in {}'.format(os.path.basename(filename)))
+	p_out=g.m.currentWindow.scatterPoints
+	np.savetxt(filename,p_out)
+	g.m.statusBar().showMessage('Successfully saved {}'.format(os.path.basename(filename)))
+	
+def load_scatter_gui():
+	filename=g.m.settings['filename']
+	if filename is not None and os.path.isfile(filename):
+		filename= QFileDialog.getOpenFileName(g.m, 'Open Scatter', filename, '*.txt')
+	else:
+		filename= QFileDialog.getOpenFileName(g.m, 'Open Scatter', '','*.txt')
+	filename=str(filename)
+	if filename=='':
+		return False
+	else:
+		load_scatter(filename)
+		
+def load_scatter(filename=None):
+	if filename is None and g.m.settings['filename'] is not None:
+		filename=g.m.settings['filename']
+	g.m.statusBar().showMessage('Loading {}'.format(os.path.basename(filename)))
+	t=time.time()
+	headers = [s.strip() for s in open(filename).readline().split('\t')]
+	x = headers.index('Xc')
+	y = headers.index('Yc')
+	z = headers.index('Zc')
+	data = np.loadtxt(filename, skiprows=1, usecols=(x, y, z))
+	g.m.statusBar().showMessage('{} successfully loaded ({} s)'.format(os.path.basename(filename), time.time()-t))
+	g.m.settings['filename']=filename
+	commands = ["open_file('{}')".format(filename)]
+	if g.m.currentWindow == None:
+		Window3D()
+	g.m.currentWindow.addScatter(data)
+	g.m.resize(1200, 800)
+	
+'''
+def load_scatter(filename):
+	g.m.statusBar().showMessage('Loading scatter from {}'.format(os.path.basename(filename)))
+	g.m.currentWindow.scatterPoints = np.loadtxt(filename)
+	g.m.currentWindow.scatterPlot.setPoints(pos=g.m.currentWindow.scatterPoints)
+	g.m.statusBar().showMessage('Successfully loaded {}'.format(os.path.basename(filename)))
+'''
+def export_nearest_distances(filename):
+	print('In progress')
+
+def export_distances(filename):
+	pts = g.m.currentWindow.scatterPoints
+	dists = []
+	for i, pt in enumerate(pts):
+		for j in range(i + 1, len(pts)):
+			dists.append(np.linalg.norm(np.subtract(pt, pts[j])))
+	np.savetxt(filename, dists, header="Distance")
