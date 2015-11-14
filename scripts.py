81b21bfaab211e276aa523eedcee256b5044ced8
Brett Settle <brettjsettle@gmail.com>
2015-11-14 01:56:07 -0800

    Script Editor Gui

--- scripts.py
+++ scripts.py
@@ -13,7 +13,8 @@ import os
 import global_vars as g
 from os.path import expanduser
 import datetime
-
+from PyQt4 import uic
+from pyqtgraph import console
 import pyqtgraph as pg
 import pyqtgraph.console
 from script_namespace import getnamespace
@@ -34,7 +35,7 @@ def getScriptList():
         script=os.path.join(g.m.scriptsDir,script)
         g.m.menuScripts.addAction(QAction("&"+name, g.m, triggered=makeFun(ScriptEditor,script)))
     g.m.menuScripts.addSeparator()
-    g.m.menuScripts.addAction(QAction("&New...", g.m, triggered=newScript))
+    g.m.menuScripts.addAction(QAction("&New...", g.m, triggered=ScriptEditor.gui))
 
 def newScript():
     filename= QFileDialog.getSaveFileName(g.m, 'Open File', g.m.scriptsDir, '*.py')
@@ -63,79 +64,42 @@ def qstr2str(string):
     string=str(string)
     return string.replace(u'\u2029','\n')
 
-    
-class ScriptEditor(QWidget):
-    def __init__(self,scriptfile):
+class Editor(QPlainTextEdit):
+    def __init__(self, parent, scriptfile = ''):
         QWidget.__init__(self)
-        self.setWindowTitle(scriptfile)
-        self.setGeometry(QRect(357, 178, 1100, 430))
-        self.editor=Editor(scriptfile,self)
-        self.interpreter=Interpreter(self)
-        self.layout=QHBoxLayout()
-        self.splitter =  QSplitter()
-        self.splitter.addWidget(self.editor)
-        self.splitter.addWidget(self.interpreter)
-        self.splitter.setSizes([500,500])
-        self.layout.addWidget(self.splitter)        
-        self.setLayout(self.layout)
-        self.show()
-        g.m.scriptEditor=self
-    def closeEvent(self, event):
-        self.editor.save()
-        g.m.scriptEditor=None
-        event.accept() # let the window close
-    
-class Editor(QWidget):
-    def __init__(self,scriptfile,parent):
-        QWidget.__init__(self)
-        self.parent=parent
-        self.scriptfile=scriptfile
-        self.textEdit=QPlainTextEdit()
+        self.parent = parent
+        if scriptfile != '':
+            self.load_file(scriptfile)
+        
+    def load_file(self, scriptfile):
+        self.scriptfile = scriptfile
         f = open(scriptfile, 'r')
         script=f.read()
         f.close()
-        self.textEdit.setPlainText(script)
-        self.saveButton=QPushButton('Save')
-        self.saveButton.clicked.connect(self.save)
-        self.runButton=QPushButton('Run')
-        self.runButton.clicked.connect(self.run)
-        self.runSelectedButton=QPushButton('Run Selected (F9)')
-        self.runSelectedButton.clicked.connect(self.runSelected)
-        self.layout=QVBoxLayout()
-        self.layout_b=QHBoxLayout()
-        self.layout.addWidget(self.textEdit)
-        self.layout_b.addWidget(self.saveButton)
-        self.layout_b.addWidget(self.runButton)
-        self.layout_b.addWidget(self.runSelectedButton)
-        self.layout.addLayout(self.layout_b)
-        self.setLayout(self.layout)
+        self.setPlainText(script)
+        self.parent.statusBar().showMessage('{} loaded.'.format(os.path.basename(self.scriptfile)))
+
+    def save_as(self):
+        filename= str(QFileDialog.getSaveFileName(g.m, 'Save script', g.m.scriptsDir, '*.py'))
+        if filename == '':
+            return
+        self.scriptfile = filename
+        self.save()
+
     def save(self):
+        if not hasattr(self, 'scriptfile'):
+            print('This script has not been saved yet, please specify a filename')
+            self.save_as()
         f = open(self.scriptfile, 'w')
-        command=qstr2str(self.textEdit.toPlainText())
+        command=qstr2str(self.toPlainText())
         f.write(command)
         f.close()
-        g.m.statusBar().showMessage('{} saved.'.format(os.path.basename(self.scriptfile)))
-    def run(self):
-        self.save()
-        command=qstr2str(self.textEdit.toPlainText())
-        self.parent.interpreter.runCmd(command)
-    def runSelected(self):
-        cursor=self.textEdit.textCursor()
-        command=cursor.selectedText() #self=g.m.scriptEditor.editor; a=self.command
-        self.command=command
-        command=qstr2str(command)
-        self.parent.interpreter.runCmd(command)
-    def keyPressEvent(self,ev):
-        if ev.key() == Qt.Key_F9:
-            self.runSelected()
-            ev.accept()
-    
+        self.parent.statusBar().showMessage('{} saved.'.format(os.path.basename(self.scriptfile)))
 
-class Interpreter(pyqtgraph.console.ConsoleWidget):
-    def __init__(self,parent):
-        self.parent=parent
-        namespace=getnamespace()
-        ## initial text to display in the console
+class ScriptEditor(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        uic.loadUi('gui/scriptEditor.ui', self)
         text = """
         This is an interactive python console. The numpy and pyqtgraph modules have already been imported 
         as 'np' and 'pg'.
@@ -146,16 +110,109 @@ class Interpreter(pyqtgraph.console.ConsoleWidget):
             - You can get traces of those rois by using the roi.getTrace() function
         
         """
-        pyqtgraph.console.ConsoleWidget.__init__(self,namespace=namespace,text=text)
+        self.consoleWidget.localNamespace=getnamespace()
+        self.consoleWidget.output.setPlainText(text)
+        self.addEditor()
+        self.saveButton.clicked.connect(self.saveCurrentScript)
+        self.runButton.clicked.connect(self.runScript)
+        self.runSelectedButton.clicked.connect(self.runSelected)
+        self.actionNew_Script.triggered.connect(lambda f: self.addEditor())
+        self.actionFrom_File.triggered.connect(self.importScript)
+        self.actionFrom_Window.triggered.connect(lambda : self.currentTab().setPlainText('\n'.join(g.m.currentWindow.commands)))
+        self.actionSave_Script.triggered.connect(self.saveCurrentScript)
+        g.m.scriptEditor = self
+        self.eventeater = ScriptEventEater(self)
+        self.setAcceptDrops(True)
+        self.installEventFilter(self.eventeater)
+        self.scriptTabs.tabCloseRequested.connect(self.closeTab)
+
+    def closeTab(self, index):
+        self.scriptTabs.removeTab(index)
+
+    def saveCurrentScript(self):
+        cw = self.currentTab()
+        if cw == None:
+            return
+        cw.save()
+        self.scriptTabs.setTabText(cw.currentIndex(), os.path.basename(cw.scriptfile))
+
+    def currentTab(self):
+        return self.scriptTabs.currentWidget()
+
+    def importScript(self, scriptfile=''):
+        if scriptfile == '':
+            scriptfile= str(QFileDialog.getOpenFileName(self, 'Load script', g.m.scriptsDir, '*.py'))
+            if scriptfile == '':
+                return
+        cw = self.currentTab()
+        cw.load_file(scriptfile)
+        self.scriptTabs.setTabText(self.scriptTabs.currentIndex(), os.path.basename(cw.scriptfile))
+    
+    def addEditor(self, scriptfile=''):
+        self.setUpdatesEnabled(False)
+        e = Editor(self, scriptfile=scriptfile)
+        e.keyPressEvent = lambda ev: self.editorKeyPressEvent(e, ev)
+        if scriptfile == '':
+            scriptfile = 'New Script'
+        self.scriptTabs.insertTab(0, e, scriptfile)
+        self.scriptTabs.setCurrentIndex(0)
+        self.setUpdatesEnabled(True)
+
+    def keyPressEvent(self, ev):
+        if ev.key() == Qt.Key_N and ev.modifiers() == Qt.ControlModifier:
+            self.addEditor()
+            ev.accept()
+
+    def editorKeyPressEvent(self, editor, ev):
+        if ev.key() == Qt.Key_F9:
+            self.runSelected()
+            ev.accept()
+        else:
+            Editor.keyPressEvent(editor, ev)
+
+    def runScript(self):
+        if self.currentTab() == None:
+            return
+        command=qstr2str(self.currentTab().toPlainText())
+        self.consoleWidget.runCmd(command)
+
+    def runSelected(self):
+        if self.currentTab() == None:
+            return
+        cursor=self.currentTab().textCursor()
+        command=cursor.selectedText() #self=g.m.scriptEditor.editor; a=self.command
+        self.command=command
+        command=qstr2str(command)
+        self.consoleWidget.runCmd(command)
+
+    @staticmethod
+    def gui():
+        if g.m.scriptEditor == None:
+            g.m.scriptEditor = ScriptEditor()
+        g.m.scriptEditor.show()
 
 
-if __name__ == "__main__":
-    from pyqtgraph import console
-    from PyQt4 import uic, QtCore, QtGui
-    app = QtGui.QApplication([])
-    ui = uic.loadUi('gui/scriptEditor.ui')
-    ui.show()       
-    app.exec_()     
+class ScriptEventEater(QObject):
+    def __init__(self,parent=None):
+        QObject.__init__(self,parent)
+        self.parent = parent
+    def eventFilter(self,obj,event):
+        if (event.type()==QEvent.DragEnter):
+            if event.mimeData().hasUrls():
+                event.accept()   # must accept the dragEnterEvent or else the dropEvent can't occur !!!
+            else:
+                event.ignore()
+        if (event.type() == QEvent.Drop):
+            if event.mimeData().hasUrls():   # if file or link is dropped
+                url = event.mimeData().urls()[0]   # get first url
+                filename=url.toString()
+                filename=filename.split('file:///')[1]
+                print('filename={}'.format(filename))
+                self.parent.importScript(filename)  #This fails on windows symbolic links.  http://stackoverflow.com/questions/15258506/os-path-islink-on-windows-with-python
+                event.accept()
+            else:
+                event.ignore()
+        return False # lets the event continue to the edit
 
     
     
