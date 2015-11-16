fd6c21a9149666912be4925fabfcb13da7bcc537
kyleellefsen <kellefse@uci.edu>
2015-11-16 14:14:24 -0800

    updated detect_puffs name

--- plugins/detect_puffs/__init__.py
+++ plugins/detect_puffs/__init__.py
@@ -7,7 +7,7 @@ Created on Mon Jul 21 10:32:00 2014
 
 dependencies = ['skimage', 'leastsqbound', 'matplotlib','PyOpenGL']
 
-menu_layout = {'Puffs': \
+menu_layout = {'detect_puffs': \
 				{'Average Origin': ['average_origin', 'average_origin.gui'], \
 				'Frame By Frame': ['frame_by_frame_origin', 'frame_by_frame.gui'],\
 				'Threshold Cluster': ['threshold_cluster', 'threshold_cluster.gui']}}
