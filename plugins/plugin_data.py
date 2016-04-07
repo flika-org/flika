e92b6500b67880669b23d1f761503dc7ba6230e6
Brett Settle <brettjsettle@gmail.com>
2016-04-07 15:32:57 -0700

    name error
    plugin_data must match xml

--- plugins/plugin_data.py
+++ plugins/plugin_data.py
@@ -3,5 +3,5 @@ plugin_list = {
 'Beam Splitter': 'https://raw.githubusercontent.com/BrettJSettle/BeamSplitter/master/info.xml',
 'Rodent Tracker': 'https://raw.githubusercontent.com/kyleellefsen/rodentTracker/master/info.xml',
 'Detect Puffs': 'https://raw.githubusercontent.com/kyleellefsen/detect_puffs/master/info.xml',
-'Drift Correct': 'https://raw.githubusercontent.com/BrettJSettle/DriftCorrection/master/info.xml'
+'Drift Correction': 'https://raw.githubusercontent.com/BrettJSettle/DriftCorrection/master/info.xml'
 }
