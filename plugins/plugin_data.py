7e97b1bfa7dfb7ed2bb53b67624a6fe3aed6c797
Brett Settle <brettjsettle@gmail.com>
2016-04-07 15:23:35 -0700

    added drift correct

--- plugins/plugin_data.py
+++ plugins/plugin_data.py
@@ -2,5 +2,6 @@ plugin_list = {
 'Global Analysis': 'https://raw.githubusercontent.com/BrettJSettle/GlobalAnalysisPlugin/master/info.xml',
 'Beam Splitter': 'https://raw.githubusercontent.com/BrettJSettle/BeamSplitter/master/info.xml',
 'Rodent Tracker': 'https://raw.githubusercontent.com/kyleellefsen/rodentTracker/master/info.xml',
-'Detect Puffs': 'https://raw.githubusercontent.com/kyleellefsen/detect_puffs/master/info.xml'
+'Detect Puffs': 'https://raw.githubusercontent.com/kyleellefsen/detect_puffs/master/info.xml',
+'Drift Correct': 'https://raw.githubusercontent.com/BrettJSettle/DriftCorrection/master/info.xml',
 }
