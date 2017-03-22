a87d7a506f936ec2a213244d86edfcb18b230830
Kyle Ellefsen <kellefse@uci.edu>
2017-03-21 19:25:19 -0700

    bug fix

--- flika/logger.py
+++ flika/logger.py
@@ -2,7 +2,7 @@ from logging import getLogger, basicConfig
 import os
 LOG_DIR = os.path.join(os.path.expanduser("~"), '.FLIKA', 'log')
 if not os.path.exists(LOG_DIR):
-    os.mkdir(LOG_DIR)
+    os.makedirs(LOG_DIR)
 LOG_PATH = os.path.join(LOG_DIR, 'FLIKALOG.log')
 basicConfig(filename=LOG_PATH)
 logger = getLogger("flika")
