b402cee4ad8ed3294e4b0f3b14a9f3c476573aba
Kyle Ellefsen <kellefse@uci.edu>
2017-09-20 15:13:54 -0700

    added 'imports' file
    from flika.utils.imports import *

--- flika/__init__.py
+++ flika/__init__.py
@@ -1,19 +1,6 @@
 from .logger import logger
 logger.debug("Started 'reading __init__.py'")
-
-
+from .version import __version__
 from .flika import start_flika
 from . import global_vars as g
-#from .window import Window
-from .version import __version__
-#from .process import *
-#from .roi import open_rois
-
-
-# import modules that are commonly used inside flika
-#import numpy as np
-#from pyqtgraph import plot, show
-#import pyqtgraph as pg
-#import pyqtgraph.opengl as gl
-
 logger.debug("Completed 'reading __init__.py'")
