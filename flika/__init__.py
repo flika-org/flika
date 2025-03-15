e8d071ec190f8aa9df42cd6706994547cd61a813
Kyle Ellefsen <kyleellefsen@gmail.com>
2025-03-14 22:55:47 -0700

    moves version import and adds beartype to build requirements

--- flika/__init__.py
+++ flika/__init__.py
@@ -2,9 +2,13 @@
 Flika: An interactive image processing program for biologists written in Python.
 """
 
+# First, get the version directly
+from flika.version import __version__
+
+# After the version is imported, import other modules
+# This prevents circular imports during setup
 from flika.logger import logger
 logger.debug("Started 'reading __init__.py'")
-from flika.version import __version__
 from flika.flika import start_flika
 import flika.global_vars as g
 logger.debug("Completed 'reading __init__.py'")
